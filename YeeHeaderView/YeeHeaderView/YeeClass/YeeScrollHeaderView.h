b29374a804efb4f3df0d019e281817e0648c34dd
CoderYee <13137880636@163.com>
2017-03-15 17:59:59 +0800

    commit

---
+++ YeeHeaderView/YeeHeaderView/YeeClass/YeeScrollHeaderView.h
@@ -0,0 +1,13 @@
+//
+//  YeeScrollHeaderView.h
+//  YeeHeaderView
+//
+//  Created by CoderYee on 2017/3/15.
+//  Copyright © 2017年 CoderYee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YeeScrollHeaderView : UIScrollView
+
+@end
---
+++ YeeHeaderView/YeeHeaderView/YeeClass/YeeScrollHeaderView.m
@@ -0,0 +1,21 @@
+//
+//  YeeScrollHeaderView.m
+//  YeeHeaderView
+//
+//  Created by CoderYee on 2017/3/15.
+//  Copyright © 2017年 CoderYee. All rights reserved.
+//
+
+#import "YeeScrollHeaderView.h"
+
+@implementation YeeScrollHeaderView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
