5eab720805690fd6660079b329a844f04c800c73
CoderYee <13137880636@163.com>
2017-05-12 19:16:12 +0800

    VideoDemo commit

---
+++ YeeVideoTestDemo/YeeVideoTestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YeeVideoTestDemo
+//
+//  Created by CoderYee on 2017/5/12.
+//  Copyright © 2017年 CodeYee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
