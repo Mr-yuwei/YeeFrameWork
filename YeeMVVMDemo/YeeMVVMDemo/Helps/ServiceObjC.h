d2cf6b547c0b9fb9d0ccc18bb05274fd7ae4f04a
CoderYee <13137880636@163.com>
2017-05-15 19:53:17 +0800

    commit

---
+++ YeeMVVMDemo/YeeMVVMDemo/Helps/ServiceObjC.h
@@ -0,0 +1,15 @@
+//
+//  Service.h
+//  YeeMVVMDemo
+//
+//  Created by CoderYee on 2017/5/15.
+//  Copyright © 2017年 CodeYee. All rights reserved.
+//  这里存放网络请求,返回的信号
+
+#import <Foundation/Foundation.h>
+#import "ReactiveObjC.h "
+@interface ServiceObjC : NSObject
+
++(RACSignal*)postRequestWithUrl:(NSString *)Url parameters:(NSDictionary *)parameters;
+
+@end
---
+++ YeeMVVMDemo/YeeMVVMDemo/Helps/ServiceObjC.m
@@ -0,0 +1,33 @@
+//
+//  Service.m
+//  YeeMVVMDemo
+//
+//  Created by CoderYee on 2017/5/15.
+//  Copyright © 2017年 CodeYee. All rights reserved.
+//
+
+#import "ServiceObjC.h"
+#import "AFNetworking.h"
+@implementation ServiceObjC
+
++(RACSignal*)postRequestWithUrl:(NSString *)Url parameters:(NSDictionary *)parameters{
+
+    return [[RACSignal createSignal:^RACDisposable * _Nullable(id<RACSubscriber>  _Nonnull subscriber) {
+        
+     [ServiceObjC postWithUrlString:Url parameters:parameters SuccessBlock:^(NSDictionary *dic) {
+         
+            
+        } failBlock:^(NSError *error) {
+           
+            
+        }];
+    }] replayLazily] ;
+}
+#pragma mark --post请求数据接口
++ (void)postWithUrlString:(NSString *)urlString parameters:(NSDictionary*)parameters SuccessBlock:(void(^)(NSDictionary*dic))successBlock failBlock:(void(^)(NSError*error))failBlock{
+    
+    
+    
+    
+}
+@end
