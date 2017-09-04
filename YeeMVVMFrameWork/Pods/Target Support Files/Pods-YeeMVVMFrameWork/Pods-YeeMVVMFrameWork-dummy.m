336c9cf857c167531e815e6eb84209ad4283a00e
Yee <13137880636@163.com>
2017-09-04 18:48:43 +0800

    Create    YeeFrameWork

---
+++ YeeMVVMFrameWork/Pods/Target Support Files/Pods-YeeMVVMFrameWork/Pods-YeeMVVMFrameWork-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_YeeMVVMFrameWork : NSObject
+@end
+@implementation PodsDummy_Pods_YeeMVVMFrameWork
+@end
---
+++ YeeMVVMFrameWork/YeeMVVMFrameWork/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YeeMVVMFrameWork
+//
+//  Created by Yee on 2017/9/4.
+//  Copyright © 2017年 1bu2bu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
